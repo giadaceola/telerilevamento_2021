e93762c2312ced043ecdb182450bda80eab61e05
giadaceola <80513552+giadaceola@users.noreply.github.com>
2021-03-17 12:57:44 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -13,3 +13,12 @@ p224r63_2011 <- brick("p224r63_2011_masked.grd")
 #leggo i dati di questo raster e lo visualizzo
 p224r63_2011
 plot(p224r63_2011)
+
+#cambio la scala di colori con 3 caratteri (colori), i quali possono assumere 100 valori
+cl<-colorRampPalette(c("black","grey","light grey")) (100)
+plot(p224r63_2011, col=cl)
+
+#ulteriore cambio della scala di colori
+cln<-colorRampPalette(c("blue","green","red","yellow","pink")) (100)
+plot(p224r63_2011, col=cln)
+

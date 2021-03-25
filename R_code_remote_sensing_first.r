6c111991dcb31c981fe4d6f762053a34b90b1eb2
giadaceola <80513552+giadaceola@users.noreply.github.com>
2021-03-25 12:42:30 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -22,3 +22,63 @@ plot(p224r63_2011, col=cl)
 cln<-colorRampPalette(c("blue","green","red","yellow","pink")) (100)
 plot(p224r63_2011, col=cln)
 
+# Bande Landsat
+# B1: blu
+# B2: verde
+# B3: rosso
+# B4: infrarosso vicino
+# B5: infrarosso medio
+# B6: infrarosso termico
+# B7: infrarosso medio
+
+#ripulisco la finestra grafica
+dev.off()
+
+#lego la banda 1 all'immagine satellitare totale e la plotto con i colori predefinti di R
+plot(p224r63_2011$B1_sre)
+#plotto la banda 1 utilizzando la color ramp palette che ho precedentemente definito (cln)
+cln<-colorRampPalette(c("blue","green","red","yellow","pink")) (100)
+plot(p224r63_2011$B1_sre, col=cln)
+
+dev.off()
+
+#preparo una finestra grafica multiframe in cui i plot sono disposti in 1 riga, 2 colonne
+par(mfrow=c(1,2))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+
+#2 righe, 1 colonna
+par(mfrow=c(2,1))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+
+#plotto le prime quattro bande di Landsat in 4 righe, 1 colonna
+par(mfrow=c(4,1))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+plot(p224r63_2011$B3_sre)
+plot(p224r63_2011$B4_sre)
+
+#2 righe, 2 colonne
+par(mfrow=c(2,2))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+plot(p224r63_2011$B3_sre)
+plot(p224r63_2011$B4_sre)
+
+#plotto le quattro bande in disposizione 2x2, ognuna con la propria palette
+par(mfrow=c(2,2))
+
+clb <- colorRampPalette(c("dark blue","blue","light blue")) (100)
+plot(p224r63_2011$B1_sre, col=clb)
+
+clg <- colorRampPalette(c("dark green","green","light green")) (100)
+plot(p224r63_2011$B2_sre, col=clg)
+
+clr <- colorRampPalette(c("dark red","red","pink")) (100)
+plot(p224r63_2011$B3_sre, col=clr)
+
+clnir <- colorRampPalette(c("red","orange","yellow")) (100)
+plot(p224r63_2011$B4_sre, col=clnir)
+
+

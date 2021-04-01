3dc889e9692aa5c0f5eae9ef6a22f65eaa31d55e
giadaceola <80513552+giadaceola@users.noreply.github.com>
2021-04-01 13:16:27 +0200

    Create R_code_time_series.r

---
+++ R_code_time_series.r
@@ -0,0 +1,12 @@
+#Time series analysis
+#Aumento di temperatura in Groenlandia
+#Dati e codice di Emanuela Cosma
+
+# install.packages("raster")
+library(raster)
+
+# definisco la working directory per Windows
+setwd("C:/lab/greenland")
+# setwd("~/lab/greenland") # Linux
+# setwd("/Users/name/Desktop/lab/greenland") # Mac
+

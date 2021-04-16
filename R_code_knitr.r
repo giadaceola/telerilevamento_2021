740273bf13b2914ead024ee2c1b86286ca358a70
giadaceola <80513552+giadaceola@users.noreply.github.com>
2021-04-16 10:57:24 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -9,3 +9,8 @@ setwd("C:/lab/") # Windows
 # knitr prende un codice all'esterno di R, e all'interno di R genera un report (che viene salvato nella cartella del codice)
 # richiamo la funzione knitr
 library(knitr)
+
+# la funzione stitch prende il codice di riferimento e, utilizzando il pacchetto knitr, genera il pdf come output
+stitch("R_code_greenland.r.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+
+

457bfb765b5c894923b8a3c4e5942d3dd0fc4d35
giadaceola <80513552+giadaceola@users.noreply.github.com>
2021-04-28 12:37:21 +0200

    Update R_code_multivariate_analysis.r

--- R_code_multivariate_analysis.r
+++ R_code_multivariate_analysis.r
@@ -27,4 +27,29 @@ pairs(p224r63_2011)
 # se sono molto correlate l'indice ha una dimensione dei caratteri più grande
 # se c'è grande correlazione, possimo usare l'analisi multivariata per ridurre il numero di bande nel nostro sistema conservando la stessa informazione 
 
+# Ricampionamento (resampling)
+# poiché la PCA è piuttosto pesante, creo un'immagine alleggerita con la funzione aggregate
+# con "aggregate" diminuisco la risoluzione di 10 volte linearmente (fact=10), quindi aggrego le celle
+# lo associo ad un oggetto (suffisso res mi indica che è stato fatto un resampling)
+p224r63_2011res <- aggregate(p224r63_2011, fact=10)
+p224r63_2011res
+# visualizzo il risultato, confrontandolo con l'originale, in RGB in cui la banda dell'infrarosso la associo alla componente red
+par(mfrow=c(2,1))
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="lin")
+plotRGB(p224r63_2011res, r=4, g=3, b=2, stretch="lin")
+
+# PCA - Principal Components Analysis
+# utilizzo la funzione rasterPCA
+p224r63_2011res_pca <- rasterPCA(p224r63_2011res)
+# leggo le informazioni dell'oggetto
+p224r63_2011res_pca
+# guardo il sommario del modello, in cui vedo quanta varianza spiegano le varie bande (componenti)
+summary(p224r63_2011res_pca$model)
+# plotto la mappa generata dalla funzione rasterPCA
+plot(p224r63_2011res_pca$map)
+
+# plotto in RGB le prime 3 componenti principali (che spiegano quasi il totale della varianza)
+plotRGB(p224r63_2011res_pca$map, r=1, g=2, b=3, stretch="lin")
+
+
 

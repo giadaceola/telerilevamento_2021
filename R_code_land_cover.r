709f2af3e8ff87d65db10875fe55ac2ab52d9129
giadaceola <80513552+giadaceola@users.noreply.github.com>
2021-05-07 10:45:18 +0200

    Update R_code_land_cover.r

--- R_code_land_cover.r
+++ R_code_land_cover.r
@@ -35,6 +35,61 @@ p1 <- ggRGB(defor1, r=1, g=2, b=3, stretch="lin")
 p2 <- ggRGB(defor2, r=1, g=2, b=3, stretch="lin")
 grid.arrange(p1, p2, nrow=2)
 
+# classificazione non supervisionata, con 2 classi
+d1c <- unsuperClass(defor1, nClasses=2)
+# la classe della foresta amazzonica è di colore verde (2), la parte agricola di colore bianco (1)
+# set.seed()
+# plotto la mappa
+plot(d1c$map)
+# stessa cosa per defor2
+d2c <- unsuperClass(defor2, nClasses=2)
+plot(d2c$map)
+# la classe della foresta amazzonica è di colore bianco (1), la parte agricola di colore verde (2)
+ # classificazione non supervisionata con 3 classi
+d2c3 <- unsuperClass(defor2, nClasses=3)
+plot(d2c3$map)
+
+# calcolo la frequenza dei pixel di una certa classe
+freq(d1c$map)
+#     value  count
+# [1,]     1  34784
+# [2,]     2 306508
+ # faccio la somma dei pixel, corrispondente ai pixel totali
+s1 <- 34784 + 306508 
+s1
+# faccio la proporzione (frequenza/totale)
+prop1 <- freq(d1c$map)/s1
+prop1
+# prop foresta: 0.8980814
+# prop agricoltura: 0.1019186
+
+# stessa cosa per la seconda immagine, che prò ha un numero totale di pixel diverso
+s2 <- 342726
+prop2 <- freq(d2c$map) / s2
+prop2
+# prop foresta: 0.5185396
+# prop agricoltura: 0.4814604
+
+# genero un dataframe
+# prima colonna
+cover <- c("Forest","Agriculture")
+# seconda colonna
+percent_1992 <- c(89.80, 10.19)
+percent_2006 <- c(51.85, 48.14)
+# le unisco in un dataframe
+percentages <- data.frame(cover, percent_1992, percent_2006)
+percentages
+
+# creo un grafico di questo dataframe con ggplot2
+# colore in base alle classi
+# con l'argomento geom_bar creo delle barre
+# dati che indico io, quindi "identity"
+ggplot(percentages, aes(x=cover, y=percent_1992, color=cover)) + geom_bar(stat="identity", fill="white")
+ggplot(percentages, aes(x=cover, y=percent_2006, color=cover)) + geom_bar(stat="identity", fill="white")
+# metto i due grafici in una finestra unica
+p1 <- ggplot(percentages, aes(x=cover, y=percent_1992, color=cover)) + geom_bar(stat="identity", fill="white")
+p2 <- ggplot(percentages, aes(x=cover, y=percent_2006, color=cover)) + geom_bar(stat="identity", fill="white")
+grid.arrange(p1,p2, nrow=1)
 
 
 

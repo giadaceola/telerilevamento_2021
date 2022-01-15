c28c71c07eec9bfb48838e63720dbe3dc8cee758
giadaceola <80513552+giadaceola@users.noreply.github.com>
2022-01-15 12:21:05 +0100

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -35,6 +35,9 @@ plot(dan2021$T35TPK_20210731T085601_B03, col=clg, main="Banda verde")
 plot(dan2021$T35TPK_20210731T085601_B04, col=clr, main="Banda rosso")
 plot(dan2021$T35TPK_20210731T085601_B08, col=clnir, main="Banda nir")
 
+#library(ggplot2)
+#library(gridExtra)
+#library(RStoolbox)
 p321<-ggRGB(dan2021, 3,2,1, stretch="lin")
 p421<-ggRGB(dan2021, 4,2,1, stretch="lin")
 grid.arrange(p321, p421, nrow = 1)
@@ -50,10 +53,10 @@ p418<-ggRGB(dan2018, 4,2,1, stretch="lin")
 grid.arrange(p318, p418, nrow = 1)
 
 par(mfrow=c(2,2))
-plotRGB(dan2018, 3,2,1, stretch="lin")
-plotRGB(dan2021, 3,2,1, stretch="lin")
-plotRGB(dan2018, 4,2,1, stretch="lin")
-plotRGB(dan2021, 4,2,1, stretch="lin")
+plotRGB(dan2018, 3,2,1, stretch="lin", axes=TRUE, main= "2018 RGB:3,2,1")
+plotRGB(dan2021, 3,2,1, stretch="lin", axes=TRUE, main= "2021 RGB:3,2,1")
+plotRGB(dan2018, 4,2,1, stretch="lin", axes=TRUE, main= "2018 RGB:4,2,1")
+plotRGB(dan2021, 4,2,1, stretch="lin", axes=TRUE, main= "2021 RGB:4,2,1")
 
 dan2021r <- aggregate(dan2021, fact=50)
 plot(dan2021r)
@@ -62,35 +65,46 @@ par(mfrow=c(2,1))
 plotRGB(dan2021, 4,2,1, stretch="lin")
 plotRGB(dan2021r, 4,2,1, stretch="lin")
 
-pairs(dan2021r)
+pairs(dan2021r, main="Correlazioni tra variabili")
+
+### Analisi delle componenti principali
+# RStoolbox
 dan2021r_pca<-rasterPCA(dan2021r)
 dan2021r_pca
 summary(dan2021r_pca$model)
 plot(dan2021r_pca$map)
-plotRGB(dan2021r_pca$map, r=1, g=2, b=3, stretch="lin")
 
-# variabilità locale
+plotRGB(dan2021r_pca$map, r=1, g=2, b=3, stretch="lin", axes=TRUE, main="Mappa r=PC1, g=PC2, b=PC3")
+
+### Variabilità locale
+# NDVI 2021
 nir21<-dan2021r$T35TPK_20210731T085601_B08
 red21<-dan2021r$T35TPK_20210731T085601_B04
 ndvi21 <- (nir21-red21) / (nir21+red21)
 plot(ndvi21, main="NDVI 2021")
 
+# moving window 3x3 della deviazione standard di ndvi21
+#raster
 ndvi21sd3 <- focal(ndvi21, w=matrix(1/9, nrow=3, ncol=3), fun=sd)
 clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100)
-plot(ndvi21sd3, col=clsd)
+plot(ndvi21sd3, col=clsd, main="Deviazione standard di NDVI21")
 
+# moving window 3x3 della deviazione standard nella PC1
 pc121 <- dan21r_pca$map$PC1
 pc121sd3 <- focal(pc121, w=matrix(1/9, nrow=3, ncol=3), fun=sd)
-plot(pc121sd3, col=clsd)
+plot(pc121sd3, col=clsd, main="Deviazione standard su PC1")
 dev.off()
 
+#library(ggplot2)
+#library(viridis)
 ggplot() + geom_raster(pc121sd3, mapping = aes(x = x, y = y, fill = layer)) + scale_fill_viridis()
 dev.off()
 
-# differenza NDVI
-# calcolo prima anche NDVI 2018
+### Differenza NDVI
+# calcolo prima anche NDVI 2018 su immagine ricampionata
 dan2018r <- aggregate(dan2018, fact=50)
 
+# NDVI 2018
 nir18<-dan2018r$T35TPK_20180717T085601_B08
 red18<-dan2018r$T35TPK_20180717T085601_B04
 ndvi18 <- (nir18-red18) / (nir18+red18)
@@ -102,23 +116,28 @@ plot(ndvi18, main="NDVI 2018")
 plot(ndvi21, main="NDVI 2021")
 dev.off()
 
+# differenza tra ndvi 2021 e ndvi 2018
 difndvi <- ndvi21 - ndvi18
 cld <- colorRampPalette(c('red','white','green'))(100)
 plot(difndvi, col=cld, main="Differenza tra NDVI del 2021 e NDVI del 2018")
 dev.off()
 
-# spectral indices
+### Spectral indices
+
 # b1= blue
 # b2= green
 # b3= red
 # b4= nir
 
 clsi <- colorRampPalette(c('black','darkblue','red','yellow'))(100)
+
+#RStoolbox
 spind <- spectralIndices(dan2021r, blue=1, green=2, red=3, nir=4)
 plot(spind, col=clsi)
 dev.off()
 
-# classificazione non supervisionata
+### Classificazione non supervisionata
+#RStoolbox
 dan2021rc3 <- unsuperClass(dan2021r, nClasses=3)
 plot(dan2021rc3$map, main="Classificazione non supervisionata con 3 classi")
 dev.off()
@@ -134,7 +153,7 @@ plot(dan2021rc10$map)
 plotRGB(dan2021, 3,2,1, stretch="lin")
 dev.off()
 
-#calcolo la frequenza dei pixel di una classe
+### Calcolo della frequenza dei pixel di una certa classe
 freq(dan2021rc3$map)
 #     value count
 #[1,]     1 10228
@@ -161,7 +180,8 @@ percentages <- data.frame(cover,percent21)
 ggplot(percentages, aes(x=cover, y=percent21, color=cover)) + geom_bar(stat="identity", fill="white")+ ggtitle("% copertura 2021")
 dev.off()
 
-# firme spettrali
+### Firme spettrali
+#raster
 plotRGB(dan2021, 3,2,1, stretch="lin")
 click(dan2021, id=T, xy=T, cell=T, type="p", pch=16, col="yellow")
 
@@ -169,31 +189,26 @@ plotRGB(dan2018, 3,2,1, stretch="lin")
 click(dan2018, id=T, xy=T, cell=T, type="p", pch=16, col="yellow")
 
 band<-c(1,2,3,4)
-pixel12021<-c(230, 427, 269, 3698)
-pixel22021<-c(927, 1312, 2072, 2802)
-pixel32021<-c(336, 621, 420, 2824)
-pixel42021<-c(483,1116,687,442)
-pixel12018<-c(897, 772, 447, 3869)
-pixel22018<-c(1014, 1113, 682, 3956)
-pixel32018<-c(882, 886, 475, 3024)
-pixel42018<-c(1052,1161,712,350)
-water2021<-c(365, 323, 258, 193)
-water2018<-c(884, 607, 376, 221)
+pixel1_2021<-c(230, 427, 269, 3698)
+pixel2_2021<-c(927, 1312, 2072, 2802)
+pixel1_2018<-c(897, 772, 447, 3869)
+pixel2_2018<-c(1014, 1113, 682, 3956)
+water_2021<-c(365, 323, 258, 193)
+water_2018<-c(884, 607, 376, 221)
 
-fs<-data.frame(band, pixel12021, pixel12018, pixel22021, pixel22018, pixel32021, pixel32018, pixel42021, pixel42018, water2021, water2018)
+fs<-data.frame(band, pixel1_2021, pixel1_2018, pixel2_2021, pixel2_2018, water_2021, water_2018)
 fs
 
 ggplot(fs, aes(x=band))+
-geom_line(aes(y=pixel12021), color="green") +
-geom_line(aes(y=pixel12018), color="green") +
-geom_line(aes(y=pixel22021), color="red") +
-geom_line(aes(y=pixel22018), color="red") +
-geom_line(aes(y=pixel32021), color="brown") +
-geom_line(aes(y=pixel32018), color="brown") +
-geom_line(aes(y=pixel42021), color="black") +
-geom_line(aes(y=pixel42018), color="black") +
-geom_line(aes(y=water2021), color="blue")+
-labs(x="band",y="reflectance")
+geom_line(aes(y=pixel1_2021, colour="pixel1_2021")) +
+geom_line(aes(y=pixel1_2018, colour="pixel1_2018"))+
+geom_line(aes(y=pixel2_2021, colour="pixel2_2021")) +
+geom_line(aes(y=pixel2_2018, colour="pixel2_2018")) +
+geom_line(aes(y=water_2021, colour="water_2021"))+
+geom_line(aes(y=water_2018, colour="water_2018"))+
+scale_colour_manual("", breaks = c("pixel1_2021","pixel1_2018","pixel2_2021","pixel2_2018","water_2021","water_2018"),values = c("dark green","green","red","magenta","dark blue","blue")) +
+labs(x="band",y="reflectance", title="Firme spettrali")
+
 
 
 
